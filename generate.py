a8df4e189ce2a0ccd46942d6d1c50449d1f10a8f
anvuong <anvb@vng.com.vn>
2023-05-23 16:36:55 -0700

    add Brick object

--- generate.py
+++ generate.py
@@ -13,56 +13,50 @@ if __name__ == "__main__":
         color_dict = json.load(f)
 
     # ldr_filename = "./ldr_files/dataset/2blocks-perpendicular_15.ldr"
-    ldr_filename = "./ldr_files/dataset/wall_augmented270_18.ldr"
+    # ldr_filename = "./ldr_files/dataset/wall_augmented270_18.ldr"
     # ldr_filename = "./ldr_files/block_fake.ldr"
     # ldr_filename = "./ldr_files/2bricks_cross.ldr"
     # ldr_filename = "./ldr_files/5bricks_rotate.ldr"
     # ldr_filename = "./ldr_files/7bricks_rotate.ldr"
-    print(ldr_filename)
+    ldr_filename = "./ldr_files/different_bricks.ldr"
+    print(f"constructing model for {ldr_filename}")
 
     columns = ["line_type", "color_code", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "file_block"]
     df = pd.read_csv(ldr_filename, sep=" ", names=columns)
     df["block_size"] = df["file_block"].apply(lambda x: helpers.decode_file_block(x))
 
-    all_vertices_list = []
     plotly_data = []
     opacity = 0.5
     marker_size = 3
     line_width = 4
 
     for idx, row in df.iterrows():
-        tm = np.array([[row.a, row.b, row.c, row.x],
-                    [row.d, row.e, row.f, row.y],
-                    [row.g, row.h, row.i, row.z],
-                    [0, 0, 0, 1]])
-        print(tm)
-        vertices = helpers.build_vertices(0, 0, 0, df.block_size[idx])
-        print(vertices.shape, vertices)
-        vertices = np.hstack([vertices, np.ones((8,1))])
-        vertices = np.matmul(tm, vertices.T)
-        print(vertices.shape, vertices)
-        vertices = vertices[:3,:].T
-        all_vertices_list += list(vertices)
-        print(vertices.shape, vertices)
-        print('\n\n')
+        # get default translation matrix from ldr
+        tm = helpers.build_translation_matrix(row.x, row.y, row.z, 
+                                              row.a, row.b, row.c, 
+                                              row.d, row.e, row.f, 
+                                              row.g, row.h, row.i)
 
-        plotly_data += helpers.build_plotly(vertices, opacity, color_dict, row.color_code, marker_size, line_width)
+        # construct a default brick
+        brick = helpers.Brick(row.x, row.y, row.z, 
+                            tm, 
+                            row.file_block,
+                            color_dict[str(row.color_code)])
+
+        # apply test transformation
+        # if idx == 8:
+            # brick.rotate(30)
+            # brick.translate(40, 24, -20)
+            # brick.unit_translate(2,1,-1)
+
+        # get 3d coordinates
+        vertices = brick.get_vertices()
+
+        plotly_data += brick.build_plotly(opacity, marker_size, line_width)
         
-
-    fig = go.Figure(data=plotly_data)
-
-    # fig.update_layout(
-    #     scene = dict(
-    #         xaxis = dict(nticks=4, range=[-100,100],),
-    #                      yaxis = dict(nticks=4, range=[-50,100],),
-    #                      zaxis = dict(nticks=4, range=[-100,100],),),
-    #     width=700,
-    #     margin=dict(r=20, l=10, b=10, t=10))
-    fig.update_layout(
-        scene = dict(
-            aspectratio=dict(x=1, y=1, z=1), # <---- tried this too
-            aspectmode='cube'
-        ),
-        # template='plotly_dark',
-    )
+    layout = go.Layout(
+                scene=dict(
+                aspectmode='data'
+            ))
+    fig = go.Figure(data=plotly_data, layout=layout)
     fig.show()
--- helpers.py
+++ helpers.py
@@ -1,22 +1,175 @@
 import numpy as np
 import plotly.graph_objects as go
 
+class Brick(object):
+    def __init__(self, x, y, z, default_transform_matrix, block_type, color_hex="#ffffff", lego_unit_length=20, lego_unit_height=24):
+        self.block_type = block_type
+        self.block_size = decode_file_block(block_type)
+        self.color_hex = color_hex
+        self.unit_length = lego_unit_length
+        self.unit_height = lego_unit_height
+        self.tm = default_transform_matrix
+        self.transformation_list = []
+
+        # these coordinates are at the center of the brick
+        self.init_x = x
+        self.init_y = y
+        self.init_z = z
+
+        # initialize numpy array of vertices
+        self.vertices = build_vertices(0, 0, 0, self.block_size, lego_unit_length=self.unit_length, lego_unit_height=self.unit_height)
+        self.apply_transformation(self.tm)
+
+    def rotate(self, angle):
+        # translate the center to origin, pivot point
+        self.translate(-self.init_x, -self.init_y, -self.init_z)
+
+        #rotate
+        tm = np.array([[np.cos(angle*np.pi/180), 0, -np.sin(angle*np.pi/180), 0],
+                     [0, 1, 0, 0],
+                     [np.sin(angle*np.pi/180), 0, np.cos(angle*np.pi/180), 0],
+                     [0, 0, 0, 1]])
+        self.apply_transformation(tm)
+
+        # translate the center back, pivot point
+        self.translate(self.init_x, self.init_y, self.init_z)
+
+    def translate(self, x, y, z):
+        tm = np.array([[1, 0, 0, x],
+                     [0, 1, 0, y],
+                     [0, 0, 1, z],
+                     [0, 0, 0, 1]])
+        self.apply_transformation(tm)
+
+    def unit_translate(self, x, y, z):
+        # this is the same as translate() but the unit is scaled to lego dimension
+        # x, z (width, depth) unit increment of 20
+        # -y (height) unit increment of 24
+        self.translate(x*self.unit_length, y*self.unit_height, z*self.unit_length)
+
+    def apply_transformation(self, tm):
+        vertices = np.hstack([self.vertices, np.ones((8,1))])
+        vertices = np.matmul(tm, vertices.T)
+        self.vertices = vertices[:3,:].T
+        self.transformation_list.append(tm)
+
+    def get_transformation_history(self):
+        return self.transformation_list
+
+    def get_vertices(self):
+        return self.vertices
+    
+    def _build_plotly_data(self, x, y, z, opacity, line_color, marker_size=2, line_width=4):
+        d = go.Scatter3d(x=x,
+                        y=y,
+                        z=z,
+                        opacity=opacity,
+                        mode='markers+lines',
+                        line={"color":line_color, "width":line_width},
+                        marker={"size":marker_size}
+                        )
+        return d
+    
+    def build_plotly(self, opacity=0.5, marker_size=3, line_width=4):
+        plotly_data = []
+        # build plotly data
+        x_vertices_upper = [self.vertices[0][0], self.vertices[1][0], self.vertices[2][0], self.vertices[3][0], self.vertices[0][0]]
+        y_vertices_upper = [self.vertices[0][1], self.vertices[1][1], self.vertices[2][1], self.vertices[3][1], self.vertices[0][1]]
+        z_vertices_upper = [self.vertices[0][2], self.vertices[1][2], self.vertices[2][2], self.vertices[3][2], self.vertices[0][2]]
+        plotly_data.append(self._build_plotly_data(x_vertices_upper, 
+                                            y_vertices_upper, 
+                                            z_vertices_upper, 
+                                            opacity, self.color_hex, marker_size, line_width))
+        
+        x_vertices_lower = [self.vertices[4][0], self.vertices[5][0], self.vertices[6][0], self.vertices[7][0], self.vertices[4][0]]
+        y_vertices_lower = [self.vertices[4][1], self.vertices[5][1], self.vertices[6][1], self.vertices[7][1], self.vertices[4][1]]
+        z_vertices_lower = [self.vertices[4][2], self.vertices[5][2], self.vertices[6][2], self.vertices[7][2], self.vertices[4][2]]
+        plotly_data.append(self._build_plotly_data(x_vertices_lower, 
+                                            y_vertices_lower, 
+                                            z_vertices_lower, 
+                                            opacity, self.color_hex, marker_size, line_width))
+        
+        x_vertices_col1 = [self.vertices[0][0], self.vertices[4][0]]
+        y_vertices_col1 = [self.vertices[0][1], self.vertices[4][1]]
+        z_vertices_col1 = [self.vertices[0][2], self.vertices[4][2]]
+        plotly_data.append(self._build_plotly_data(x_vertices_col1, 
+                                            y_vertices_col1, 
+                                            z_vertices_col1, 
+                                            opacity, self.color_hex, marker_size, line_width))
+
+        x_vertices_col2 = [self.vertices[1][0], self.vertices[5][0]]
+        y_vertices_col2 = [self.vertices[1][1], self.vertices[5][1]]
+        z_vertices_col2 = [self.vertices[1][2], self.vertices[5][2]]
+        plotly_data.append(self._build_plotly_data(x_vertices_col2, 
+                                            y_vertices_col2, 
+                                            z_vertices_col2, 
+                                            opacity, self.color_hex, marker_size, line_width))
+
+        x_vertices_col3 = [self.vertices[2][0], self.vertices[6][0]]
+        y_vertices_col3 = [self.vertices[2][1], self.vertices[6][1]]
+        z_vertices_col3 = [self.vertices[2][2], self.vertices[6][2]]
+        plotly_data.append(self._build_plotly_data(x_vertices_col3, 
+                                            y_vertices_col3, 
+                                            z_vertices_col3, 
+                                            opacity, self.color_hex, marker_size, line_width))
+
+        x_vertices_col4 = [self.vertices[3][0], self.vertices[7][0]]
+        y_vertices_col4 = [self.vertices[3][1], self.vertices[7][1]]
+        z_vertices_col4 = [self.vertices[3][2], self.vertices[7][2]]
+        plotly_data.append(self._build_plotly_data(x_vertices_col4, 
+                                            y_vertices_col4, 
+                                            z_vertices_col4, 
+                                            opacity, self.color_hex, marker_size, line_width))
+        
+        return plotly_data
+
+
 def decode_file_block(f):
+    # print(f)
     if f.lower() == "3001.dat":
         return [2,4]
+    if f.lower() == "3002.dat":
+        return [2,3]
+    if f.lower() == "3003.dat":
+        return [2,2]
+    if f.lower() == "3004.dat":
+        return [1,2]
+    if f.lower() == "3006.dat":
+        return [2,10]
+    if f.lower() == "3008.dat":
+        return [1,8]
     return [-1,-1]
 
-def build_vertices(x, y, z, block_size):
+def get_translation_matrix(x, y, z):
+    return np.array([[1, 0, 0, x],
+                     [0, 1, 0, y],
+                     [0, 0, 1, z],
+                     [0, 0, 0, 1]])
+
+def get_rotation_matrix_yaxis(angle):
+    # rotate around y axis
+    return np.array([[np.cos(angle*np.pi/180), 0, -np.sin(angle*np.pi/180), 0],
+                     [0, 1, 0, 0],
+                     [np.sin(angle*np.pi/180), 0, np.cos(angle*np.pi/180), 0],
+                     [0, 0, 0, 1]])
+
+def build_translation_matrix(x, y, z, a, b, c, d, e, f, g, h, i):
+    return np.array([[a, b, c, x],
+                     [d, e, f, y],
+                     [g, h, i, z],
+                     [0, 0, 0, 1]])
+
+def build_vertices(x, y, z, block_size, lego_unit_length=20, lego_unit_height=24):
     # ldr use x, z, -y coordinates (-y is positive up, z is depth)
-    v = [[x-block_size[1]*20/2, y, z-block_size[0]*20/2],
-         [x+ block_size[1]*20/2 , y, z-block_size[0]*20/2 ],
-         [x+ block_size[1]*20/2, y, z+ block_size[0]*20/2],
-         [x-block_size[1]*20/2, y, z+ block_size[0]*20/2], # end upper surface
+    v = [[x-block_size[1]*lego_unit_length/2, y, z-block_size[0]*lego_unit_length/2],
+         [x+ block_size[1]*lego_unit_length/2 , y, z-block_size[0]*lego_unit_length/2 ],
+         [x+ block_size[1]*lego_unit_length/2, y, z+ block_size[0]*lego_unit_length/2],
+         [x-block_size[1]*lego_unit_length/2, y, z+ block_size[0]*lego_unit_length/2], # end upper surface
          
-         [x-block_size[1]*20/2, y+24, z-block_size[0]*20/2],
-         [x+ block_size[1]*20/2 , y+24, z-block_size[0]*20/2 ],
-         [x+ block_size[1]*20/2, y+24, z+ block_size[0]*20/2],
-         [x-block_size[1]*20/2, y+24, z+ block_size[0]*20/2]] # end lower surface
+         [x-block_size[1]*lego_unit_length/2, y+lego_unit_height, z-block_size[0]*lego_unit_length/2],
+         [x+ block_size[1]*lego_unit_length/2 , y+lego_unit_height, z-block_size[0]*lego_unit_length/2 ],
+         [x+ block_size[1]*lego_unit_length/2, y+lego_unit_height, z+ block_size[0]*lego_unit_length/2],
+         [x-block_size[1]*lego_unit_length/2, y+lego_unit_height, z+ block_size[0]*lego_unit_length/2]] # end lower surface
     return np.array(v).reshape((8,3))
 
 def build_plotly_data(x, y, z, opacity, line_color, marker_size=2, line_width=4):
