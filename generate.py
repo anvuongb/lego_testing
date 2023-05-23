12dcf7ab962071e0f1337b4335177c55ddcc7d47
anvuong <anvb@vng.com.vn>
2023-05-23 15:07:04 -0700

    change plotting to plotly for faster render

--- generate.py
+++ generate.py
@@ -1,134 +1,68 @@
 import numpy as np
 import pandas as pd
 import json
+import plotly.graph_objects as go
+import helpers
 import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.use('TkAgg')
 
-def decode_file_block(f):
-    if f.lower() == "3001.dat":
-        return [2,4]
-    return [-1,-1]
+if __name__ == "__main__":
+    # load color dict
+    with open("color_codes.json", "r") as f:
+        color_dict = json.load(f)
 
-def build_vertices(x, y, z, block_size):
-    # v = [[x, y, z],
-    #      [x+ block_size[1]*20 , y, z ],
-    #      [x+ block_size[1]*20, y, z+ block_size[0]*20],
-    #      [x, y, z+ block_size[0]*20], # end upper surface
-    #      [x, y+24, z],
-    #      [x+ block_size[1]*20, y+24, z ],
-    #      [x+ block_size[1]*20, y+24, z+ block_size[0]*20],
-    #      [x, y+24, z+ block_size[0]*20]] # end lower surface
-    
-    # ldr use x, z, -y coordinates (-y is positive up, z is depth)
-    v = [[x-block_size[1]*20/2, y, z-block_size[0]*20/2],
-         [x+ block_size[1]*20/2 , y, z-block_size[0]*20/2 ],
-         [x+ block_size[1]*20/2, y, z+ block_size[0]*20/2],
-         [x-block_size[1]*20/2, y, z+ block_size[0]*20/2], # end upper surface
-         
-         [x-block_size[1]*20/2, y+24, z-block_size[0]*20/2],
-         [x+ block_size[1]*20/2 , y+24, z-block_size[0]*20/2 ],
-         [x+ block_size[1]*20/2, y+24, z+ block_size[0]*20/2],
-         [x-block_size[1]*20/2, y+24, z+ block_size[0]*20/2]] # end lower surface
-    return np.array(v).reshape((8,3))
+    # ldr_filename = "./ldr_files/dataset/2blocks-perpendicular_15.ldr"
+    ldr_filename = "./ldr_files/dataset/wall_augmented270_18.ldr"
+    # ldr_filename = "./ldr_files/block_fake.ldr"
+    # ldr_filename = "./ldr_files/2bricks_cross.ldr"
+    # ldr_filename = "./ldr_files/5bricks_rotate.ldr"
+    # ldr_filename = "./ldr_files/7bricks_rotate.ldr"
+    print(ldr_filename)
 
-# load color dict
-with open("color_codes.json", "r") as f:
-    color_dict = json.load(f)
+    columns = ["line_type", "color_code", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "file_block"]
+    df = pd.read_csv(ldr_filename, sep=" ", names=columns)
+    df["block_size"] = df["file_block"].apply(lambda x: helpers.decode_file_block(x))
 
-# ldr_filename = "./ldr_files/dataset/2blocks-perpendicular_15.ldr"
-# ldr_filename = "./ldr_files/dataset/wall_augmented270_18.ldr"
-# ldr_filename = "./ldr_files/block_fake.ldr"
-ldr_filename = "./ldr_files/2bricks_cross.ldr"
-# ldr_filename = "./ldr_files/5bricks_rotate.ldr"
-# ldr_filename = "./ldr_files/7bricks_rotate.ldr"
-print(ldr_filename)
+    all_vertices_list = []
+    plotly_data = []
+    opacity = 0.5
+    marker_size = 3
+    line_width = 4
 
-columns = ["line_type", "color_code", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "file_block"]
-df = pd.read_csv(ldr_filename, sep=" ", names=columns)
-df["block_size"] = df["file_block"].apply(lambda x: decode_file_block(x))
-# print(df)
+    for idx, row in df.iterrows():
+        tm = np.array([[row.a, row.b, row.c, row.x],
+                    [row.d, row.e, row.f, row.y],
+                    [row.g, row.h, row.i, row.z],
+                    [0, 0, 0, 1]])
+        print(tm)
+        vertices = helpers.build_vertices(0, 0, 0, df.block_size[idx])
+        print(vertices.shape, vertices)
+        vertices = np.hstack([vertices, np.ones((8,1))])
+        vertices = np.matmul(tm, vertices.T)
+        print(vertices.shape, vertices)
+        vertices = vertices[:3,:].T
+        all_vertices_list += list(vertices)
+        print(vertices.shape, vertices)
+        print('\n\n')
 
-translation_matrices = []
+        plotly_data += helpers.build_plotly(vertices, opacity, color_dict, row.color_code, marker_size, line_width)
+        
 
-# ldraw use x -z y coordinate
+    fig = go.Figure(data=plotly_data)
 
-for idx, row in df.iterrows():
-    tm = np.array([[row.a, row.b, row.c, row.x],
-                   [row.d, row.e, row.f, row.y],
-                   [row.g, row.h, row.i, row.z],
-                   [0, 0, 0, 1]])
-    translation_matrices.append(tm)
-
-origin = np.array([0,0,0,1]).reshape(4, 1)
-
-fig = plt.figure()
-ax = fig.add_subplot(projection='3d')
-ax.scatter(origin[0], origin[1], origin[2], marker="x", color="red")
-
-color_list = ['b', 'g', 'r', 'c', 'm', 'y']
-# color_list = ['r']
-
-all_vertices_list = []
-for idx, row in df.iterrows():
-    tm = np.array([[row.a, row.b, row.c, row.x],
-                   [row.d, row.e, row.f, row.y],
-                   [row.g, row.h, row.i, row.z],
-                   [0, 0, 0, 1]])
-    print(tm)
-    vertices = build_vertices(0, 0, 0, df.block_size[idx])
-    print(vertices.shape, vertices)
-    vertices = np.hstack([vertices, np.ones((8,1))])
-    vertices = np.matmul(tm, vertices.T)
-    print(vertices.shape, vertices)
-    vertices = vertices[:3,:].T
-    all_vertices_list += list(vertices)
-    print(vertices.shape, vertices)
-    print('\n\n')
-    for id, v in enumerate(vertices):
-        ax.scatter(v[0], v[1], v[2], color=color_dict[str(row.color_code)])
-        ax.plot([vertices[id][0], vertices[(id+1)%4+4*int(id>=4)][0]],
-                [vertices[id][1], vertices[(id+1)%4+4*int(id>=4)][1]],
-                [vertices[id][2], vertices[(id+1)%4+4*int(id>=4)][2]],
-                color=color_dict[str(row.color_code)])
-        if id < 4:
-            ax.plot([vertices[id][0], vertices[(id+4)][0]],
-                    [vertices[id][1], vertices[(id+4)][1]],
-                    [vertices[id][2], vertices[(id+4)][2]],
-                    color=color_dict[str(row.color_code)])
-ax.legend()
-
-# first pass clean vertices
-all_vertices_count = []
-for v in all_vertices_list:
-    tmp_count = 0
-    for k in all_vertices_list:
-        if np.array_equal(v, k):
-            tmp_count += 1
-    all_vertices_count.append(tmp_count)
-all_vertices_count = np.array(all_vertices_count)
-print(all_vertices_count)
-print(all_vertices_count < 2)
-all_vertices_set = np.array(all_vertices_list)[all_vertices_count < 2]
-
-
-# for id, v in enumerate(all_vertices_set):
-#     ax.scatter(v[0], v[1], v[2], color=color_list[idx%len(color_list)])
-
-xlim = ax.get_xlim3d()
-ylim = ax.get_ylim3d()
-zlim = ax.get_zlim3d()
-ax.set_xlabel('x-axis', fontsize=20)
-ax.set_ylabel('y-axis', fontsize=20)
-ax.set_zlabel('z-axis', fontsize=20)
-ax.set_box_aspect((xlim[1]-xlim[0], ylim[1]-ylim[0], zlim[1]-zlim[0]))
-
-# while True:
-#     for a in range(-50,50):
-#         for angle in range(0, 360):
-#             ax.view_init(a, angle)
-#             plt.draw()
-#             plt.pause(.001)
-
-# # ax.view_init(-60, 90)
-plt.show()
+    # fig.update_layout(
+    #     scene = dict(
+    #         xaxis = dict(nticks=4, range=[-100,100],),
+    #                      yaxis = dict(nticks=4, range=[-50,100],),
+    #                      zaxis = dict(nticks=4, range=[-100,100],),),
+    #     width=700,
+    #     margin=dict(r=20, l=10, b=10, t=10))
+    fig.update_layout(
+        scene = dict(
+            aspectratio=dict(x=1, y=1, z=1), # <---- tried this too
+            aspectmode='cube'
+        ),
+        # template='plotly_dark',
+    )
+    fig.show()
---
+++ helpers.py
@@ -0,0 +1,85 @@
+import numpy as np
+import plotly.graph_objects as go
+
+def decode_file_block(f):
+    if f.lower() == "3001.dat":
+        return [2,4]
+    return [-1,-1]
+
+def build_vertices(x, y, z, block_size):
+    # ldr use x, z, -y coordinates (-y is positive up, z is depth)
+    v = [[x-block_size[1]*20/2, y, z-block_size[0]*20/2],
+         [x+ block_size[1]*20/2 , y, z-block_size[0]*20/2 ],
+         [x+ block_size[1]*20/2, y, z+ block_size[0]*20/2],
+         [x-block_size[1]*20/2, y, z+ block_size[0]*20/2], # end upper surface
+         
+         [x-block_size[1]*20/2, y+24, z-block_size[0]*20/2],
+         [x+ block_size[1]*20/2 , y+24, z-block_size[0]*20/2 ],
+         [x+ block_size[1]*20/2, y+24, z+ block_size[0]*20/2],
+         [x-block_size[1]*20/2, y+24, z+ block_size[0]*20/2]] # end lower surface
+    return np.array(v).reshape((8,3))
+
+def build_plotly_data(x, y, z, opacity, line_color, marker_size=2, line_width=4):
+    d = go.Scatter3d(x=x,
+                    y=y,
+                    z=z,
+                    opacity=opacity,
+                    mode='markers+lines',
+                    line={"color":line_color, "width":line_width},
+                    marker={"size":marker_size}
+                    )
+    return d
+
+
+def build_plotly(vertices, opacity, color_dict, color_code, marker_size, line_width):
+    plotly_data = []
+    # build plotly data
+    x_vertices_upper = [vertices[0][0], vertices[1][0], vertices[2][0], vertices[3][0], vertices[0][0]]
+    y_vertices_upper = [vertices[0][1], vertices[1][1], vertices[2][1], vertices[3][1], vertices[0][1]]
+    z_vertices_upper = [vertices[0][2], vertices[1][2], vertices[2][2], vertices[3][2], vertices[0][2]]
+    plotly_data.append(build_plotly_data(x_vertices_upper, 
+                                         y_vertices_upper, 
+                                         z_vertices_upper, 
+                                         opacity, color_dict[str(color_code)], marker_size, line_width))
+    
+    x_vertices_lower = [vertices[4][0], vertices[5][0], vertices[6][0], vertices[7][0], vertices[4][0]]
+    y_vertices_lower = [vertices[4][1], vertices[5][1], vertices[6][1], vertices[7][1], vertices[4][1]]
+    z_vertices_lower = [vertices[4][2], vertices[5][2], vertices[6][2], vertices[7][2], vertices[4][2]]
+    plotly_data.append(build_plotly_data(x_vertices_lower, 
+                                         y_vertices_lower, 
+                                         z_vertices_lower, 
+                                         opacity, color_dict[str(color_code)], marker_size, line_width))
+    
+    x_vertices_col1 = [vertices[0][0], vertices[4][0]]
+    y_vertices_col1 = [vertices[0][1], vertices[4][1]]
+    z_vertices_col1 = [vertices[0][2], vertices[4][2]]
+    plotly_data.append(build_plotly_data(x_vertices_col1, 
+                                         y_vertices_col1, 
+                                         z_vertices_col1, 
+                                         opacity, color_dict[str(color_code)], marker_size, line_width))
+
+    x_vertices_col2 = [vertices[1][0], vertices[5][0]]
+    y_vertices_col2 = [vertices[1][1], vertices[5][1]]
+    z_vertices_col2 = [vertices[1][2], vertices[5][2]]
+    plotly_data.append(build_plotly_data(x_vertices_col2, 
+                                         y_vertices_col2, 
+                                         z_vertices_col2, 
+                                         opacity, color_dict[str(color_code)], marker_size, line_width))
+
+    x_vertices_col3 = [vertices[2][0], vertices[6][0]]
+    y_vertices_col3 = [vertices[2][1], vertices[6][1]]
+    z_vertices_col3 = [vertices[2][2], vertices[6][2]]
+    plotly_data.append(build_plotly_data(x_vertices_col3, 
+                                         y_vertices_col3, 
+                                         z_vertices_col3, 
+                                         opacity, color_dict[str(color_code)], marker_size, line_width))
+
+    x_vertices_col4 = [vertices[3][0], vertices[7][0]]
+    y_vertices_col4 = [vertices[3][1], vertices[7][1]]
+    z_vertices_col4 = [vertices[3][2], vertices[7][2]]
+    plotly_data.append(build_plotly_data(x_vertices_col4, 
+                                         y_vertices_col4, 
+                                         z_vertices_col4, 
+                                         opacity, color_dict[str(color_code)], marker_size, line_width))
+    
+    return plotly_data
