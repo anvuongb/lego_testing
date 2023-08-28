1f9092f055a78a9f6389a48364ed19efaf3e367e
anvuong <anvb@vng.com.vn>
2023-08-27 22:42:11 -0700

    add 'valid' convo mode

--- rand_tower_pyramid.py
+++ rand_tower_pyramid.py
@@ -10,11 +10,12 @@ from stud_control import get_all_possible_placements, update_occupied_stud_matrx
 import sys
 
 if __name__ == "__main__":
+    PLACEMENT_MODE = "valid"
     # get args
-    output_dir = None
+    output_dir = "."
     if len(sys.argv) > 1:
         output_dir = sys.argv[1]
-    output_filename = "pyramid.ldr"
+    output_filename = "pyramid"
     if len(sys.argv) > 2:
         output_filename = sys.argv[2]
     opacity = 0.5
@@ -23,18 +24,20 @@ if __name__ == "__main__":
 
     # bricks_bank = ["2456.dat", "3001.dat", "3002.dat", "3003.dat", "3004.dat", "3006.dat", "3008.dat"]
     bricks_bank = ["3002.dat", "3003.dat", "3004.dat", "3005.dat"]
+    # bricks_bank = ["3004.dat"]
     bricks_bank_budget = {
         "3001.dat":100, 
         "3002.dat":100, 
-        "3003.dat":100, 
+        "3003.dat":200, 
         "3004.dat":100, 
         "3005.dat":100
     }
+    # bricks_bank_budget = {"3004.dat":200}
     colors_bank = [60, 62, 64, 66, 68, 72, 75, 77, 79, 81, 83, 85, 87, 302, 339, 52, 285, 324, 273]
     # colors_bank = [35]
     default_tm = helpers.build_translation_matrix(0, -24, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1)
 
-    bricks_height = 10
+    bricks_height = 5
     if len(sys.argv) > 3:
         bricks_height = ast.literal_eval(sys.argv[3])
     bricks_per_level = sum(bricks_bank_budget.values())
@@ -76,7 +79,7 @@ if __name__ == "__main__":
                         lev_brick.translate_corner_to_brick_relative(base_brick)
                         # lev_brick.unit_translate(idx_w, 0, idx_w)
 
-                        placements_list = get_all_possible_placements(curr_layer.layer_stud_mat, np.ones(lev_brick.stud_matrix.shape))
+                        placements_list = get_all_possible_placements(curr_layer.layer_stud_mat, np.ones(lev_brick.stud_matrix.shape), mode=PLACEMENT_MODE)
                         if len(placements_list) > 0:
                             # randomly pick placement
                             # xunit, zunit = placements_list[np.random.randint(0,len(placements_list))]
--- stud_control.py
+++ stud_control.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy.signal import correlate2d
 
-def get_all_possible_placements(stud_mat1, stud_mat2):
+def get_all_possible_placements(stud_mat1, stud_mat2, mode='full'):
     '''
     Create a list of all possible translation (no rotation) of Brick2
     to fit on top of Brick1
@@ -9,16 +9,19 @@ def get_all_possible_placements(stud_mat1, stud_mat2):
     stud_mat2: stud matrix of brick 2
     return: a list of possible unit translations for brick 2
     '''
-    corr = correlate2d(stud_mat1, stud_mat2, mode='full', boundary='fill', fillvalue=0.0)
+    corr = correlate2d(stud_mat1, stud_mat2, mode=mode, boundary='fill', fillvalue=0.0)
     # print("corr matrix")
     # print(corr)
     row_idx, col_idx = np.where(corr == 0)
     if len(row_idx) == 0 or len(col_idx) == 0:
         return []
     # print(list(zip(row_idx, col_idx)))
-
-    row_idx = -(row_idx - stud_mat1.shape[0] + 1) # reverse since matrix representation is different than coordinate representation for this direction
-    col_idx = col_idx - stud_mat2.shape[1] + 1
+    if mode == "full":
+        row_idx = -(row_idx - stud_mat1.shape[0] + 1) # reverse since matrix representation is different than coordinate representation for this direction
+        col_idx = col_idx - stud_mat2.shape[1] + 1
+    if mode == "valid":
+        row_idx = stud_mat1.shape[0]-stud_mat2.shape[0]-row_idx  # reverse since matrix representation is different than coordinate representation for this direction
+        col_idx = col_idx
     # print(list(zip(row_idx, col_idx)))
     return list(zip(col_idx, row_idx))
 
@@ -32,6 +35,9 @@ def update_occupied_stud_matrx(stud_mat, top_mat, xunit, zunit):
 
     colmin_orig = 0
     colmax_orig = top_mat.shape[1]
+
+    # print(rowmax_orig, rowmin_orig)
+    # print(colmin_orig, colmax_orig)
 
     rowmin, rowmax = np.clip(rowmin_orig - zunit, a_min=0, a_max=stud_mat.shape[0]), np.clip(rowmax_orig - zunit, a_min=0, a_max=stud_mat.shape[0])
     colmin, colmax = np.clip(colmin_orig + xunit, a_min=0, a_max=stud_mat.shape[1]), np.clip(colmax_orig + xunit, a_min=0, a_max=stud_mat.shape[1])
